dc114057b9bf14e46b6c1329c717528722a313ec
yaniva-ws <71215994+yaniva-ws@users.noreply.github.com>
2021-06-01 09:29:50 +0300

    Update sharedLib.groovy

--- vars/sharedLib.groovy
+++ vars/sharedLib.groovy
@@ -1,107 +1,107 @@
 def call(Map pipelineParams) {
-  
-  pipeline {
-      agent { label 'slave' }
 
-      tools { maven 'maven 3.6.3' }
+    pipeline {
+        agent { label 'slave' }
 
-      // runs once a day
-      triggers {
-          cron('H H * * *')
-      }
+        tools { maven 'maven 3.6.3' }
 
-      parameters {        
-          booleanParam(
-                  name: 'RELEASE_FLAG',
-                  defaultValue: false,
-                  description: 'Check this to release a version'
-          )
-      }
+        // runs once a day
+        triggers {
+            cron('H H * * *')
+        }
 
-    environment {
-          PATH_2_POM = pipelineParams.path_to_pom
-    }
+        parameters {
+            booleanParam(
+                    name: 'RELEASE_FLAG',
+                    defaultValue: false,
+                    description: 'Check this to release a version'
+            )
+        }
 
-      stages{
-          stage ('clean and clone') {
-              steps {
-                  cleanWs()
-                  git branch: pipelineParams.branch,
-                      credentialsId: 'whitesource-github-user',
-                      url: pipelineParams.gitUrl
-              }
-          }
-          stage('Build') {
-              steps {
-                  sh 'cd ${PATH_2_POM} && mvn clean compile'
-              }
-          }
-          stage('Test') {
-              steps {
-                  sh 'cd ${PATH_2_POM} && mvn test'
-              }
-          }
-          stage('Deploy') {
-              steps {
-                  script {
-                      if (params.RELEASE_FLAG) {
-                          sh """
+        environment {
+            PATH_2_POM = pipelineParams.path_to_pom
+        }
+
+        stages{
+            stage ('clean and clone') {
+                steps {
+                    cleanWs()
+                    git branch: pipelineParams.branch,
+                            credentialsId: 'whitesource-github-user',
+                            url: pipelineParams.gitUrl
+                }
+            }
+            stage('Build') {
+                steps {
+                    sh 'cd ${PATH_2_POM} && mvn clean compile'
+                }
+            }
+            stage('Test') {
+                steps {
+                    sh 'cd ${PATH_2_POM} && mvn test'
+                }
+            }
+            stage('Deploy') {
+                steps {
+                    script {
+                        if (params.RELEASE_FLAG) {
+                            sh """
                               mvn versions:set versions:commit -DremoveSnapshot                           
                           """
 
-                          script{
-                              env.TAG_VERSION = sh(script: "mvn help:evaluate -Dexpression=project.version -q -DforceStdout", returnStdout: true).trim()
-                          }
-                          sshagent (credentials: ['whitesource-github-user']) {
-                              sh 'git commit -am "committing release version" '
-                              sh 'git push --set-upstream origin master'
-                              sh 'git tag -a ${TAG_VERSION} -m "tagging release version" '
-                              sh 'git push origin --tags'
-                          }
+                            script{
+                                env.TAG_VERSION = sh(script: "mvn help:evaluate -Dexpression=project.version -q -DforceStdout", returnStdout: true).trim()
+                            }
+                            sshagent (credentials: ['whitesource-github-user']) {
+                                sh 'git commit -am "committing release version" '
+                                sh 'git push --set-upstream origin master'
+                                sh 'git tag -a ${TAG_VERSION} -m "tagging release version" '
+                                sh 'git push origin --tags'
+                            }
 
 
-                          sh """                            
+                            sh """                            
                               mvn deploy
                           """
 
-                          script{
-                              env.NEXT_VERSION = bumpVersion(env.TAG_VERSION)
-                          }
+                            script{
+                                env.NEXT_VERSION = bumpVersion(env.TAG_VERSION)
+                            }
 
-                          sh """                            
+                            sh """                            
                               mvn -U versions:set versions:commit -DnewVersion=${NEXT_VERSION}
                           """
-                          sshagent (credentials: ['whitesource-github-user'])  {
-                              sh 'git commit -am "committing next version snapshot version ${NEXT_VERSION}" '
-                              sh 'git push --set-upstream origin master'
-                          }
+                            sshagent (credentials: ['whitesource-github-user'])  {
+                                sh 'git commit -am "committing next version snapshot version ${NEXT_VERSION}" '
+                                sh 'git push --set-upstream origin master'
+                            }
 
-                          sh """                            
+                            sh """                            
                               mvn deploy
                           """
 
-                      } else {
-                          sh 'cd ${PATH_2_POM} && mvn deploy'
-                      }
-                  }
-              }
-          }
-      }
-      post {
-          always {
-              junit(
-                      allowEmptyResults: true,
-                      testResults: '**/surefire-reports/*.xml'
-              )
-          }
-      }
-  }
+                        } else {
+                            sh 'cd ${PATH_2_POM} && mvn deploy'
+                        }
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                junit(
+                        allowEmptyResults: true,
+                        testResults: '**/surefire-reports/*.xml'
+                )
+            }
+        }
+    }
 
-  def bumpVersion(version){
-      def versionParts = version.tokenize('.')
-      major = versionParts[0].toInteger()
-      minor = versionParts[1].toInteger()
-      patch = versionParts[2].toInteger()
-      return "${major}.${minor}.${patch+1}-SNAPSHOT"
-  }
+}
+def bumpVersion(version){
+    def versionParts = version.tokenize('.')
+    major = versionParts[0].toInteger()
+    minor = versionParts[1].toInteger()
+    patch = versionParts[2].toInteger()
+    return "${major}.${minor}.${patch+1}-SNAPSHOT"
 }
