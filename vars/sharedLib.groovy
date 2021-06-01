365f234bf6c0b7da102fdc184bdb6fb09aa1c59d
yaniva-ws <71215994+yaniva-ws@users.noreply.github.com>
2021-06-01 20:42:07 +0300

    Update sharedLib.groovy

--- vars/sharedLib.groovy
+++ vars/sharedLib.groovy
@@ -97,10 +97,3 @@ def call(Map pipelineParams) {
         }
     }
 }
-def bumpVersion(version){
-    def versionParts = version.tokenize('.')
-    major = versionParts[0].toInteger()
-    minor = versionParts[1].toInteger()
-    patch = versionParts[2].toInteger()
-    return "${major}.${minor}.${patch+1}-SNAPSHOT"
-}
